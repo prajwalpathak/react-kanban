e0e2422f5e88f3cc7fc901f3b86127f5a3405d38
Prajwal Pathak <prajwalpa12@gmail.com>
2019-07-31 14:49:56 +0530

    Added data from Airbase

--- src/app.js
+++ src/app.js
@@ -5,17 +5,4 @@ import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import axios from 'axios';
 
-
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyi2CN2ILdkHpMYl'}).base('appmXZCKuTeXwhJ8Y');
-
-base('Bugs & Issues').select({
-    view: 'Bugs by Priority'
-}).firstPage(function(err, records) {
-    if (err) { console.error(err); return; }
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('Name'));
-    });
-});
-
 ReactDOM.render(<Kanban />, document.getElementById('app'));
--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -2,11 +2,29 @@ import React from 'react';
 import Board from 'react-trello'
 import data from "../../public/data.json";
 
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'keyi2CN2ILdkHpMYl' }).base('appmXZCKuTeXwhJ8Y');
 
 export default class Kanban extends React.Component {
 
-
     state = {
+        data: []
+    }
+
+    componentDidMount() {
+        base('Bugs & Issues').select({
+        }).firstPage((err, records) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            records.forEach((record) => {
+                console.log(record._rawJson);
+                this.setState({ data: this.state.data.concat(record._rawJson) });
+            });
+        });
+
+
 
     }
 
